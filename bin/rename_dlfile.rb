7f738c84edd2e26824367749f68ea37abb64d3e6
nasu-no-suage <roundabout0828@gmail.com>
2021-04-12 08:30:10 +0900

    fix: ファイル名にスラッシュが含まれるときのバグを修正
    ファイル名やサークル名にスラッシュが含まれるとファイル名の制約よりエラーが発生した。スラッシュを分数スラッシュに変更してバグを修正

--- bin/rename_dlfile.rb
+++ bin/rename_dlfile.rb
@@ -6,5 +6,6 @@ BASEDIR = '/app/downloads'
 STDIN.each do |line|
   cid, basename = line.chomp.split("\t")
   org = File.join(BASEDIR, "#{cid}.zip")
+  basename.gsub!(%r{\/}, "\u2044") #フォワードスラッシュの代わりに分数スラッシュを使用
   FileUtils.mv(org, File.join(BASEDIR, "#{basename}.zip")) if File.file?(org)
 end
