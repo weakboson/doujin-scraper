61efca324c66aedb5cfd464e09dfe6d18863ef24
nasu-no-suage <roundabout0828@gmail.com>
2021-04-20 17:30:16 +0900

    置換する文字が増えることを見越してHashを用いて文字を置換処理するようにした
    以前のコミットe2f8c09ではスラッシュだけ置換していた。先々置換する文字が増える可能性を踏まえてメソッドを定義した上でHashを用いて文字を置換する処理に変えるよう提案されたのでそのように実装した。

--- bin/rename_dlfile.rb
+++ bin/rename_dlfile.rb
@@ -1,11 +1,24 @@
 #!/usr/bin/env ruby
 require 'fileutils'
 
+def substitute_special_letters(basename,
+                               special_letters)
+  substitute_name = basename
+  special_letters.each_key do |special_letter|
+    substitute_name = substitute_name.gsub(special_letter,
+                                           special_letters)
+  end
+  return substitute_name
+end
+
+
 BASEDIR = '/app/downloads'
+special_letters = { "/" => "\u2044"}
 
 STDIN.each do |line|
   cid, basename = line.chomp.split("\t")
   org = File.join(BASEDIR, "#{cid}.zip")
-  basename.gsub!(%r{\/}, "\u2044") #フォワードスラッシュの代わりに分数スラッシュを使用
+  basename = substitute_special_letters(basename,
+                                       special_letters)
   FileUtils.mv(org, File.join(BASEDIR, "#{basename}.zip")) if File.file?(org)
 end
