f6c62f80281af678013e57cd01e066fc086cdc80
weakboson <weakboson@gmail.com>
2022-12-30 11:19:09 +0900

    いらなかったscript削除

--- bin/setup-node.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source base.sh
-
-docker-compose build
-docker-compose run --rm -u ${UID}:${GID} puppeteer yarn install
-docker-compose run --rm -u ${UID}:${GID} puppeteer yarn run tsc
