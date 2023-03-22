e8ae3dcfea2461d2608c3c6b49c7223f12533976
weakboson <weakboson@gmail.com>
2023-03-23 02:17:07 +0900

    linted

--- bin/rename_dlfile.rb
+++ bin/rename_dlfile.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'fileutils'
 
 SPECIAL_LETTERS_MAP = {
-  "/" => "\u2044",
-  ":" => "：" # 例えばコロンも対応するとしたら
-}
+  '/' => "\u2044",
+  ':' => '：' # 例えばコロンも対応するとしたら
+}.freeze
 def substitute_special_letters(original)
   sub_name = original.dup
   SPECIAL_LETTERS_MAP.each do |k, v|
@@ -15,7 +17,7 @@ end
 
 BASEDIR = '/app/downloads'
 
-STDIN.each do |line|
+$stdin.each do |line|
   cid, basename = line.chomp.split("\t")
   org = File.join(BASEDIR, "#{cid}.zip")
   FileUtils.mv(org, File.join(BASEDIR, "#{substitute_special_letters(basename)}.zip")) if File.file?(org)
